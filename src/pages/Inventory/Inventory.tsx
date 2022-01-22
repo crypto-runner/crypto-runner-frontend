a739681743d44538f088f29de83ead8779c64ce7
Saqlain <saqlainprinters@gmail.com>
2022-01-22 16:49:17 +0500

    Inventory Inorder update

--- src/pages/Inventory/Inventory.tsx
+++ src/pages/Inventory/Inventory.tsx
@@ -10,54 +10,13 @@ import {
   Theme,
   Typography,
 } from "@mui/material";
-import { useInventory } from "src/hooks/useInventory";
-import { setUserLoading } from "src/redux/user/userReducer";
-import { useDispatch } from "react-redux";
-import { v4 as uuid } from "uuid";
-import { useHistory } from "react-router-dom";
-import { POOL_CARD_ADDRESS } from "src/config/config";
+import All from "./components/All";
+import Inorder from "./components/Inorder";
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     marginTop: 70,
     paddingBottom: 70,
-  },
-  imgContainer: {
-    width: "100%",
-    height: 300,
-    backgroundColor: "#FFB888",
-    position: "relative",
-    "& img": {
-      objectFit: "contain",
-      width: "100%",
-      height: "100%",
-    },
-  },
-
-  quantityWrapper: {
-    // display: "flex",
-    width: "max-content",
-
-    justifyContent: "center",
-    background: "white",
-    border: "2px solid black",
-    padding: "2px 5px",
-  },
-  quantityContainer: {
-    position: "absolute",
-    bottom: -20,
-    // left: "calc(50% - 100px)",
-    display: "flex",
-    justifyContent: "center",
-    width: "100%",
-  },
-  quantityText: {
-    textAlign: "center",
-    fontWeight: 600,
-    fontSize: 20,
-    "& span": {
-      color: theme.palette.primary.main,
-    },
   },
 }));
 
@@ -66,17 +25,6 @@ interface Props {}
 const Inventory: React.FC<Props> = () => {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
-  const { balance, loading } = useInventory();
-  const dispatch = useDispatch();
-  const history = useHistory();
-
-  React.useEffect(() => {
-    if (loading) {
-      dispatch(setUserLoading(true));
-    } else {
-      dispatch(setUserLoading(false));
-    }
-  }, [loading]);
 
   const handleChange = (
     event: React.SyntheticEvent<Element, Event>,
@@ -97,45 +45,11 @@ const Inventory: React.FC<Props> = () => {
         <div style={{ marginTop: 20 }}>
           <Tabs value={value} onChange={handleChange} centered>
             <Tab label="All" />
-            <Tab label="Available" />
             <Tab label="Inorder" />
-            <Tab label="Direct Sale" />
-            <Tab label="Want to Buy" />
           </Tabs>
         </div>
-        <Grid container spacing={3} style={{ marginTop: 20 }}>
-          {balance?.map((item) => (
-            <Grid key={uuid()} item xs={12} sm={6} md={4} lg={3}>
-              <div
-                style={{ position: "relative" }}
-                onClick={() =>
-                  history.push(
-                    `/order-item/${POOL_CARD_ADDRESS}/${item.tokenId}`
-                  )
-                }
-              >
-                <div className={classes.imgContainer}>
-                  <img src={item.image} alt="" />
-                  <div className={classes.quantityContainer}>
-                    <div className={classes.quantityWrapper}>
-                      <Typography className={classes.quantityText}>
-                        {item.amount} <span>Total</span>
-                      </Typography>
-                    </div>
-                  </div>
-                </div>
-                <Button
-                  color="primary"
-                  fullWidth
-                  variant="outlined"
-                  style={{ marginTop: 30 }}
-                >
-                  {item.name}
-                </Button>
-              </div>
-            </Grid>
-          ))}
-        </Grid>
+        {value === 0 && <All />}
+        {value === 1 && <Inorder />}
       </Container>
     </div>
   );
---
+++ src/pages/Inventory/components/Inorder.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import { makeStyles } from "@mui/styles";
+import { Button, Grid, Theme, Typography } from "@mui/material";
+import { useWalletProvider } from "@react-dapp/wallet";
+import { useAllListedOrdersForAddress } from "@nftvillage/marketplace-sdk";
+import { v4 as uuid } from "uuid";
+import { useHistory } from "react-router-dom";
+import { getRunner } from "src/config/cards";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {},
+  imgContainer: {
+    width: "100%",
+    height: 300,
+    backgroundColor: "#FFB888",
+    position: "relative",
+    "& img": {
+      objectFit: "contain",
+      width: "100%",
+      height: "100%",
+    },
+  },
+
+  quantityWrapper: {
+    // display: "flex",
+    width: "max-content",
+
+    justifyContent: "center",
+    background: "white",
+    border: "2px solid black",
+    padding: "2px 5px",
+  },
+  quantityContainer: {
+    position: "absolute",
+    bottom: -20,
+    // left: "calc(50% - 100px)",
+    display: "flex",
+    justifyContent: "center",
+    width: "100%",
+  },
+  quantityText: {
+    textAlign: "center",
+    fontWeight: 600,
+    fontSize: 20,
+    "& span": {
+      color: theme.palette.primary.main,
+    },
+  },
+}));
+
+interface Props {}
+
+const Inorder: React.FC<Props> = () => {
+  const classes = useStyles();
+  const history = useHistory();
+  const { account } = useWalletProvider();
+  const { orders } = useAllListedOrdersForAddress(account || "");
+  console.log(orders);
+
+  return (
+    <Grid container spacing={3} style={{ marginTop: 20 }}>
+      {orders?.map((item) => (
+        <Grid key={uuid()} item xs={12} sm={6} md={4} lg={3}>
+          <div
+            style={{ position: "relative" }}
+            onClick={() =>
+              history.push(
+                `/order-item/${item.order.asset}/${item.order.assetId}`
+              )
+            }
+          >
+            <div className={classes.imgContainer}>
+              <img
+                src={
+                  item.metadata?.image ??
+                  getRunner(item.metadata.tokenId)?.image
+                }
+                alt=""
+              />
+              <div className={classes.quantityContainer}>
+                <div className={classes.quantityWrapper}>
+                  <Typography className={classes.quantityText}>
+                    {item.metadata.price} <span>BNB</span>
+                  </Typography>
+                </div>
+              </div>
+            </div>
+            <Button
+              color="primary"
+              fullWidth
+              variant="outlined"
+              style={{ marginTop: 30 }}
+            >
+              {item.metadata.name}
+            </Button>
+          </div>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default Inorder;
