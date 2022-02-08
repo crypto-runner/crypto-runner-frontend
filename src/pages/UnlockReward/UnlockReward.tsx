c2b2658a9f22e9d282cb472a9cd8e926c6169018
abdulrafaysiddiqui <abdul.rafay.siddique@gmail.com>
2022-02-08 12:26:04 +0500

    updated buy button with sold out and disabled

--- src/pages/UnlockReward/UnlockReward.tsx
+++ src/pages/UnlockReward/UnlockReward.tsx
@@ -41,7 +41,7 @@ interface Props { }
 const UnlockReward: React.FC<Props> = () => {
   const classes = useStyles();
   const { openModal } = useContext(ModalContext);
-  const { buyPack, txPending } = useBuyPack()
+  const { buyPack, soldOut, enabled, txPending } = useBuyPack()
 
   const _buyPack = async () => {
     const txResponse = await buyPack()
@@ -100,7 +100,7 @@ const UnlockReward: React.FC<Props> = () => {
           className={classes.claimBtn}
           onClick={() => _buyPack()}
         >
-          {txPending ? 'Pending...' : 'Buy Chest'}
+          {soldOut ? 'SOLD OUT' : !enabled ? 'CLOSED' : txPending ? 'PENDING...' : 'BUY CHEST'}
         </WalletButtonBase>
       </Container>
     </div>
